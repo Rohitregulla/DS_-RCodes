dedd4ca856fb8ff08089e80431865912a19e2e7e
R.R <rohitregulla@gmail.com>
2020-07-23 20:08:05 +0100

    Add files via upload

--- Association_Rules.R
+++ Association_Rules.R
@@ -1,10 +1,10 @@
-phone_data <- read.csv(file.choose())
+phone_data <- read.csv(file.choose("/Users/rohitregulla/Downloads/ DS/Assigiments/phonedata.txt"))
 # phone_data1 <- readLines(file.choose()) # if we use readLines functions more lines will be reduced
-View(phone_data)
-str(phone_data)
+View(phonedata)
+str(phonedata)
 # converting everything into character format 
-phone_data[] <- lapply(phone_data,as.character)
-View(phone_data)
+phonedata[] <- lapply(phonedata,as.character)
+View(phonedata)
 # Creating a custom fucntion to collapse all the items in a transaction into 
 # a single sentence 
 paste_fun <- function(i){
@@ -12,13 +12,13 @@ paste_fun <- function(i){
 }
 
 # Applying the custom function
-phone_data["new_col"] <- apply(phone_data,1,paste_fun)
-View(phone_data)
+phonedata["new_col"] <- apply(phonedata,1,paste_fun)
+View(phonedata)
 
 install.packages("tm")
 # tm package is used to do text manipulation and forming DTM and TDM matrices
 library(tm)
-x <- Corpus(VectorSource(phone_data$new_col)) # Selecting the new column which
+x <- Corpus(VectorSource(phonedata$new_col)) # Selecting the new column which
 # contains all items of a transaction in a single sentence
 x <- tm_map(x,stripWhitespace)
 # Creating a TDM matrix
@@ -28,7 +28,8 @@ dtm0_df <- data.frame(as.matrix(dtm0))
 View(dtm0_df)
 
 # Association Rules 
-
+install.packages("arules")
+install.packages("arulesViz")
 library(arules)
 library(arulesViz)
 # Item Frequecy plot
@@ -51,38 +52,5 @@ inspect(rules_lift)
 ?plot
 
 plot(rules,method = "graph")
-
-
-########## Groceries Data Set #########
-library(arules)
-groceries<-read.transactions(file.choose(),format="basket")
-inspect(groceries[1:10])
-class(groceries)
-# itemFrequencyPlot can be applicable only for transaction data 
-# count of each item from all the transactions 
-itemFrequencyPlot(groceries,topN=20)
-groceries_rules<-apriori(groceries,parameter = list(support = 0.002,confidence = 0.05,minlen=3))
-
-library(arulesViz)
-plot(groceries_rules,method = "scatterplot")
-plot(groceries_rules,method = "grouped")
-plot(groceries_rules,method = "graph")
-plot(groceries_rules,method = "mosaic")
-
-
-### On inbuilt Data set #####
-library(arules)
-data("Groceries")
-summary(Groceries)
-inspect(Groceries[1:10])
-rules <- apriori(Groceries,parameter = list(support = 0.002,confidence = 0.05,minlen=5))
-inspect(rules[1:5])
-windows()
-plot(rules,method = "scatterplot")
-plot(rules,method = "grouped")
 plot(rules,method = "graph")
-plot(rules,method = "mosaic")
 
-rules <- sort(rules,by="lift")
-
-inspect(rules[1:4])
